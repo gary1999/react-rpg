b5a0cddca898d19b02a3a2e4584153d4f6526448
gary1999 <shengary1999@gmail.com>
2022-12-05 17:43:39 +1300

    Update on increment

--- src/App.js
+++ src/App.js
@@ -6,60 +6,43 @@ function App() {
     const [tin, setTin] = useState(0);
     const [copper, setCopper] = useState(0);
 
-    const resource = {
-        wood: 0,
-        tin: 0,
-        copper: 0,
-    };
-
-    // const resource = [
-    //     {
-    //         id: 1,
-    //         name: 'wood',
-    //         amount: 0,
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'tin',
-    //         amount: 0,
-    //     },
-    //     {
-    //         id: 3,
-    //         name: 'copper',
-    //         amount: 0,
-    //     },
-    //     {
-    //         id: 4,
-    //         name: 'steel',
-    //         amount: 0,
-    //     },
-    // ];
+    const resource = [
+        {
+            id: 1,
+            name: 'wood',
+            amount: 0,
+        },
+        {
+            id: 2,
+            name: 'tin',
+            amount: 0,
+        },
+        {
+            id: 3,
+            name: 'copper',
+            amount: 0,
+        },
+        {
+            id: 4,
+            name: 'steel',
+            amount: 0,
+        },
+    ];
 
     const [resources, setResources] = useState(resource);
 
-    const [resourceArray, setResourceArray] = useState([]);
-
     const incrementWood = () => {
-        setWood((wood) => wood + 1);
-        setResources({
-            ...resources,
-            wood: wood + 1,
+        const newValue = resources.map((resource) => {
+            if (resource.id === 1) {
+                return { ...resource, amount: resource.amount + 1 };
+            } else {
+                return resource;
+            }
         });
-        if (!resourceArray.includes('wood')) {
-            setResourceArray([...resourceArray, 'wood']);
-        }
+        setResources(newValue);
     };
 
-    const incrementTin = () => {
-        setTin((tin) => tin + 1);
-        setResources({
-            ...resources,
-            tin: tin + 1,
-        });
-        if (!resourceArray.includes('tin')) {
-            setResourceArray([...resourceArray, 'tin']);
-        }
-    };
+    const incrementTin = () => {};
 
     const branchTest = () => {
         //Test
@@ -78,19 +61,19 @@ function App() {
                         <button
                             onClick={incrementWood}
                         >{`Chop down tree`}</button>
-                        <button onClick={incrementTin}>{`Mine tin`}</button>
                         {/* <p>{resourceArray}</p> */}
                     </div>
 
                     <div className="inventory">
-                        <ul>
-                            {resourceArray.map((resource, index) => {
-                                return <li>{resource}</li>;
-                            })}
-                        </ul>
-
-                        <div className="dialogue-box"></div>
+                        {resources.map((resource, index) => {
+                            return (
+                                <li key={index}>
+                                    {resource.name} {resource.amount}
+                                </li>
+                            );
+                        })}
                     </div>
+                    <div className="dialogue-box"></div>
                 </div>
             </div>
         </>
