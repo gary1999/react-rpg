78f343d60436496e413236ad92c6333611cd76b6
gary1999 <shengary1999@gmail.com>
2023-11-22 10:21:19 +1300

    Encounter test

--- src/Components/TabComponent/AllTabs/MainTab.js
+++ src/Components/TabComponent/AllTabs/MainTab.js
@@ -2,10 +2,23 @@
 import React, { useState, useEffect } from 'react';
 import ItemsTab from './ItemsTab';
 
+const monsterEncounter = () => {
+    console.log('Encounter');
+};
+
 const MainTab = () => {
     return (
         <div id="main-tab">
-            <div id="main-container"></div>
+            <div id="main-container">
+                <div id="main-content">
+                    <button
+                        id="fight-monster-button"
+                        onClick={() => monsterEncounter()}
+                    >
+                        Click to seek monster
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
