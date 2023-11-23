639832fa86d44ea23b724361a34f9934a0674f8c
gary1999 <shengary1999@gmail.com>
2023-11-24 11:50:33 +1300

    Monster spawn and display as words
    Placeholder div to show monster's name when button is clicked

--- src/Components/TabComponent/AllTabs/MainTab.js
+++ src/Components/TabComponent/AllTabs/MainTab.js
@@ -3,22 +3,18 @@ import React, { useState, useEffect } from 'react';
 import monsters from '../../Lists/Monsters';
 import ItemsTab from './ItemsTab';
 
-const monsterEncounter = () => {
-    // console.log('Encounter');
-    // for (let i = 0; i < 10; i++) {
-    //     console.log(monsters[Math.floor(Math.random() * monsters.length)]);
-    // }
-
-    // let index = Object.keys(monsters).length;
-    // console.log(Math.floor(Math.random() * index));
-
-    //Generate a random monster name to display
-    console.log(
-        monsters[Math.floor(Math.random() * Object.keys(monsters).length)].name
-    );
-};
-
 const MainTab = () => {
+    const [monsterName, setMonsterName] = useState('');
+
+    const monsterEncounter = () => {
+        console.log('Encounter');
+        //Generate a random monster name to display
+        setMonsterName(
+            monsters[Math.floor(Math.random() * Object.keys(monsters).length)]
+                .name
+        );
+    };
+
     return (
         <div id="main-tab">
             <div id="main-container">
@@ -29,6 +25,9 @@ const MainTab = () => {
                     >
                         Click to seek monster
                     </button>
+                    <div id="monster-encounter">
+                        <span>{monsterName}</span>
+                    </div>
                 </div>
             </div>
         </div>
